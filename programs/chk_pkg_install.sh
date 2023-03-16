d1ca7e125465af0bffd41f44d0c4c6f42f592ed4
kevinkirst <kevin.kmk@hotmail.com>
2023-03-16 17:07:23 -0300

    created chk_pkg_install.sh

---
+++ programs/chk_pkg_install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# changelog
+#	v1.0 - check is a package is installed, name of the package must be passed as parameter when calling this script
+#		 - if running this script straight from terminal, 'source' it
+#		 - 
+
+# =-= VARIABLES =-= #
+
+# =-= FUNCTIONS =-= #
+
+# =-= MAIN =-= #
+# vVv main script code vVv
+# if initial parameter is empty, errors out
+if [[ -z "$1" ]]; then 
+	echo "chk_install: no package name was provided."
+	return 2
+fi
+# check if pkg is installed
+#	$?=0 if package is installed
+#	$?=1 if it isn't
+#	$?=2 if no parameter was provided
+if ! hash "$1" 2>/dev/null
+then
+	return 1
+else
+	echo "chk_install: $1 was already installed on this machine."
+	return 0
+fi
