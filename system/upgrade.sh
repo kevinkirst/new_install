4c8e66e1727163e0ab421a2388e1eb0c46b8bfab
kevinkirst <kevin.kmk@hotmail.com>
2023-03-17 10:26:18 -0300

    created upgrade.sh and added check if running as root to upgrade.sh and update.sh

--- system/upgrade.sh
+++ system/upgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # changelog
-#	v1.0 - script for automatic system update
+#	v1.0 - script for automatic system upgrade
 #		 - created help function
 #		 - logs successfull and error operations to separate folders
 #		 - checks if variables from config file are set, this is so the script can be used on its own instead of only being usable by sourcing through 'new_install'
@@ -21,15 +21,15 @@ if [ -z "${DIR_LOG_OK+x}" ]; then
 	cd "/etc/new_install"
 	source ./lib/config.cfg
 fi
-LOG_OK=${DIR_LOG_OK}update.log
-LOG_ERROR=${DIR_LOG_ERROR}update.log
+LOG_OK=${DIR_LOG_OK}upgrade.log
+LOG_ERROR=${DIR_LOG_ERROR}upgrade.log
 
 # =-= FUNCTIONS =-= #
 function help {
-	echo "update.sh: update.sh [--help]
-	Update the Linux system.
+	echo "upgrade.sh: upgrade.sh [--help]
+	Upgrade the Linux system.
 	
-	Execute this script to update the Linux system.
+	Execute this script to upgrade the Linux system.
 	
 	Options:
 		--help		print the help page"
@@ -48,6 +48,6 @@ if [[ "${1:-}" = "--help" ]]; then help; fi
 if ! [[ -z "${1:-}" ]]; then help; fi
 header>> ${LOG_OK}
 header>> ${LOG_ERROR}
-apt-get update -y >> ${LOG_OK} 2>> ${LOG_ERROR}
+apt-get upgrade -y >> ${LOG_OK} 2>> ${LOG_ERROR}
 echo>> ${LOG_OK}
 echo>> ${LOG_ERROR}
