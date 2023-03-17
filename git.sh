8e35b9b66797b6e0a1fcceaf8052d7b5e9160e01
kevinkirst <kevin.kmk@hotmail.com>
2023-03-17 09:55:09 -0300

    created (and used) git.sh

---
+++ git.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# changelog
+#	v1.0 - auto push to github
+#		 - usage: git.sh [commit comment]
+#		 - 
+#		 - 
+
+# =-= VARIABLES =-= #
+COMMENT=$@
+REPO=new_install
+BRANCH=main
+
+# =-= FUNCTIONS =-= #
+
+# =-= MAIN =-= #
+# vVv main script code vVv
+# check if comment parameter was passed
+if [ -z "${1+x}" ]; then
+	read -r -p "insert commit comment: " COMMENT
+fi
+git add .
+git commit -m "$COMMENT"
+git push "$REPO" "$BRANCH"
