eed5650b60797643b06515eac8875a9ad919ae2d
kevinkirst <kevin.kmk@hotmail.com>
2023-03-21 17:48:34 -0300

    improved chk_pkg_install with dpkg and better logging of already installed programs

--- programs/chk_pkg_install.sh
+++ programs/chk_pkg_install.sh
@@ -3,10 +3,27 @@
 # changelog
 #	v1.0 - check is a package is installed, name of the package must be passed as parameter when calling this script
 #		 - if running this script straight from terminal, 'source' it
+#		 - logs programs that were already installed before the script was executed to $LOG_OK
 #		 - 
 
 # =-= USAGE =-= #
 # source ${DIR_PROGRAMS}/chk_pkg_install.sh PACKAGE_NAME
+
+# =-= VARIABLES =-= #
+# check if config.cfg variable is set, sources config.cfg if not
+if [ -z "${DIR_LOG_OK+x}" ]; then 
+	cd "/etc/new_install"
+	source ./lib/config.cfg
+fi
+LOG_OK=${DIR_LOG_OK}chk_pkg_install.log
+
+# =-= FUNCTIONS =-= #
+function header {
+	echo "${DASH}
+DATE: ${MY_DATE}
+TIME: ${MY_TIME}
+"
+}
 
 # =-= MAIN =-= #
 # vVv main script code vVv
@@ -19,10 +36,16 @@ fi
 #	$?=0 if package is installed
 #	$?=1 if it isn't
 #	$?=2 if no parameter was provided
-if ! hash "$1" 2>/dev/null
+# if ! hash "$1" 2>/dev/null
+if ! dpkg -s "$1" >> "$TMP_FILE" 2> /dev/null
 then
 	return 1
 else
+	header>> "$LOG_OK"
+	cat "$TMP_FILE">> "$LOG_OK"
+	echo>> "$LOG_OK"
+	# clearing temp file so it's empty for the next package log
+	cat /dev/null > "$TMP_FILE"
 	echo "chk_install: $1 was already installed on this machine."
 	return 0
 fi
